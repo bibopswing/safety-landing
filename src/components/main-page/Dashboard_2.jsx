fcacc3e34a88a66ac0f11140300cf0555fe909e8
jaemin <yjm8440@naver.com>
2025-07-02 18:00:53 +0900

    lottie로 넣어뒀지만 첫 2초 재생 적용은 불가능 (현재 마지막 프레임에 멈추도록 설정)

--- src/components/main-page/Dashboard_2.jsx
+++ src/components/main-page/Dashboard_2.jsx
@@ -1,17 +1,15 @@
-
 import { useTranslations } from "next-intl";
 import Container from "../common/Container";
-import Image from "next/image";
-import img from "../../assets/images/Placeholder.png";
+import Lottie from "lottie-react";
+import img from "../../assets/images/main-page/lottie.json";
 import FadeIn from "../common/FadeIn";
 
 export default function Dashboard_2() {
-
   const t = useTranslations("Dashboard_2");
 
   return (
     <Container>
-      <FadeIn  start="top 80%" delay={0.3} >
+      <FadeIn start="top 80%" delay={0.3}>
         <div className="flex flex-col items-center gap-[1rem]">
           <p className="text-[1.1rem] uppercase blue">{t("colored-text")}</p>
           <h2 className="lg:max-w-[35%] text-center">{t("title")}</h2>
@@ -21,8 +19,16 @@ export default function Dashboard_2() {
           </div>
         </div>
       </FadeIn>
-      <FadeIn start="top 80%" >
-        <Image className="w-full mt-[1rem]" src={img} alt="dashboard" />
+      <FadeIn start="top 80%">
+        <div className="w-full mt-[1rem]">
+          <Lottie
+            animationData={img}
+            loop={false}
+            autoplay={true}
+            style={{ width: "100%" }}
+            onComplete={() => {}}
+          />
+        </div>
       </FadeIn>
     </Container>
   );
