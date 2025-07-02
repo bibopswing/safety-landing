dc27d209fd210772adc4e8650c03d709690ea9be
jaemin <yjm8440@naver.com>
2025-07-02 16:47:21 +0900

    image change

--- src/components/main-page/Dashboard.jsx
+++ src/components/main-page/Dashboard.jsx
@@ -1,6 +1,6 @@
 import Container from "../common/Container";
 import Image from "next/image";
-import dashboard from "../../assets/images/main-page/dashboard/Body.png";
+import dashboard from "../../assets/images/main-page/dashboard/Body2.png";
 import logo from "../../assets/images/main-page/dashboard/SAFETY.svg";
 import FadeIn from "../common/FadeIn";
 
@@ -8,10 +8,10 @@ export default function Dashboard(params) {
   return (
     <Container>
       <div className="flex flex-col items-center justify-center">
-        <FadeIn start="top 80%" delay={0.3} >
+        <FadeIn start="top 80%" delay={0.3}>
           <Image src={logo} alt="company name" />
         </FadeIn>
-        <FadeIn start="top 80%" >
+        <FadeIn start="top 80%">
           <Image
             className="translate-y-[-10%]"
             src={dashboard}
